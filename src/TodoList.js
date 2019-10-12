4e722298464d622ce219c9a005a569beca60ebb7
serena <m15029023957@163.com>
2019-10-12 17:01:14 +0800

    实现增删列表功能

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,17 +1,59 @@
 import React,{ Component,Fragment}from 'react'
 class TodoList extends Component{
+    constructor(props){
+        //构造函数,最优先执行的函数
+        super(props);//super调用父类的构造函数
+        this.state = {
+            inputValue:'',
+            list:[],
+        }
+    }
     render(){
         return (
             <Fragment>
-                <div><input /><button>提交</button></div>
+                <div>
+                    <input 
+                      value={this.state.inputValue} 
+                      onChange={this.handleInputChange.bind(this)}
+                    />
+                    <button onClick={this.handleBtnClick.bind(this)}>提交</button>
+                </div>
                 <ul>
-                    <li>f</li>
-                    <li>f</li>
-                    <li>s</li>
-                    <li>ad</li>
+                    {
+                        this.state.list.map((item,index)=>{
+                            return <li 
+                                key={index} 
+                                onClick={this.handleDelete.bind(this,index)}
+                            >{item}</li>
+                        })
+                    }
                 </ul>
             </Fragment>
         )
+    }
+    handleInputChange(event){
+        //react中改变state中的值用setState这个方法,
+        //用bind（this）对函数的作用域进行变更
+        this.setState({
+            inputValue:event.target.value
+        })
+    }
+    handleBtnClick(){
+        //展开运算符
+        this.setState({
+            list:[...this.state.list,this.state.inputValue],
+            inputValue:''
+        })
+    }
+    handleDelete(index){
+        // console.log(index);
+        //immutable
+        //state不允许我们做任何的改变
+        const list = [...this.state.list];//展开运算符
+        list.splice(index,1)
+        this.setState({
+            list:list
+        })
     }
 }
 export default TodoList;
